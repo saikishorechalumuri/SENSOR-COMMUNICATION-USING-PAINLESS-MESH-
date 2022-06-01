34cc990ffcf5a66ab0a6af6da81ff3c76fb52f6c
Sai kishore chalumuri <93335682+saikishorechalumuri@users.noreply.github.com>
2022-06-01 13:05:35 -0400

    Add files via upload

---
+++ FOURTHNODE/FOURTHNODE.ino
@@ -0,0 +1,140 @@
+#include "painlessMesh.h"
+#include <Arduino_JSON.h>
+
+#include "painlessMesh.h"
+//#include <Arduino_JSON.h>
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_MPU6050.h>
+#include <Wire.h>
+
+
+
+// MESH Details
+#define   MESH_PREFIX     "RNTMESH" //name for your MESH
+#define   MESH_PASSWORD   "MESHpassword" //password for your MESH
+#define   MESH_PORT       5555 //default port
+
+//MPU object on the default I2C pins
+Adafruit_MPU6050 mpu;
+
+//Number for this node
+int nodeNumber = 4;
+
+//String to send to other nodes with sensor readings
+String readings;
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+String getReadings(); // Prototype for sending sensor readings
+
+//Create tasks: to send messages and get readings;
+Task taskSendMessage(TASK_SECOND * 5 , TASK_FOREVER, &sendMessage);
+
+String getReadings () {
+  JSONVar jsonReadings;
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  jsonReadings["node"] = nodeNumber;
+  jsonReadings["XAXIS"] = String(a.acceleration.x);
+  jsonReadings["YAXIS"] = String(a.acceleration.y);
+  jsonReadings["ZAXIS"] = String(a.acceleration.z);
+  readings = JSON.stringify(jsonReadings);
+  return readings;
+}
+
+void sendMessage () {
+  String msg = getReadings();
+  mesh.sendBroadcast(msg);
+}
+
+//Init BME280
+void initmpu(){
+   if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+   Serial.println("");
+  delay(100);
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
+  JSONVar myObject = JSON.parse(msg.c_str());
+  int node = myObject["node"];
+  double XAXIS  = myObject["XAXIS"];
+  double YAXIS = myObject["YAXIS"];
+  double ZAXIS = myObject["ZAXIS"];
+  Serial.print("Node: ");
+  Serial.println(node);
+ /* Serial.print("X VALUE IS: ");
+  Serial.print(XAXIS);
+  Serial.println(" C");
+  Serial.print("Y VALUE IS: ");
+  Serial.print(YAXIS);
+  Serial.println(" %");
+  Serial.print("Z VALUE IS : ");
+  Serial.print(ZAXIS);
+  //Serial.println(" hpa");*/
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+  Serial.printf("New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  initmpu();
+
+  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  userScheduler.addTask(taskSendMessage);
+  taskSendMessage.enable();
+}
+
+void loop() {
+  // it will run the user scheduler as well
+  mesh.update();
+}
